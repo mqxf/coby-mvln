72d02a2f0f66e7dd003528a47427a5c8698a9e55
Maxim Savenkov <max_savenkov@mail.ru>
2022-05-18 21:44:09 +0200

    Inital commit

---
+++ src/main.s
@@ -0,0 +1,16 @@
+.LC0:
+	.string	"Hello\n"
+	.globl	main
+main:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	ret
+
