a5ddb7a7e9d805002f4c02f18243c0d22636301a
Maxim Savenkov <max_savenkov@mail.ru>
2022-05-21 00:34:31 +0200

    Co-authored-by: v22lel <v22lel@users.noreply.github.com>

--- compiler/src/include/AST.h
+++ compiler/src/include/AST.h
@@ -10,44 +10,17 @@ struct VISITOR_STRUCT;
 
 typedef struct AST_STRUCT {
     enum {
-        AST_COMPOUND,
-        AST_FUNCTION,
-        AST_CALL,
-        AST_ASSIGNMENT = 99,
-        AST_DEFINITION_TYPE,
-        AST_VARIABLE,
-        AST_STATEMENT_RETURN,
-        AST_STATEMENT_IF,
-        AST_STATEMENT_ELSE_IF,
-        AST_STATEMENT_ELSE,
-        AST_STATEMENT_SWITCH,
-        AST_STATEMENT_CASE,
-        AST_ACCESS,
-        AST_INT,
-        AST_STRING,
-        AST_CHAR,
-        AST_FLOAT,
-        AST_HEX,
-        AST_BIN,
-        AST_BINOP,
-        AST_NOOP,
+        AST_TOKEN_
     } type;
 
     dataType dtype;
   
     list_t* children;
     char* name;
-    char* stringValue;
-    struct AST_STRUCT* value;
-    struct AST_STRUCT* left;
-    struct AST_STRUCT* right;
-    int op;
+    char* strValue;
     int intValue;
-    int id;
-    int stackIndex;
-    int multiplier;
-    struct AST_STRUCT* (*fptr)(struct VISITOR_STRUCT* visitor, struct AST_STRUCT* node, list_t* list);
-    stackFrame_t* stackFrame;
+    float floatValue;
+
 } AST_t;
 
 AST_t* initAST(int type);
--- compiler/src/parser.c
+++ compiler/src/parser.c
@@ -19,78 +19,17 @@ token_t* parserEat(parser_t* parser, unsigned int type) {
 }
 
 AST_t* parserParse(parser_t* parser) {
-    return parseCompound(parser);
+    return parseFile(parser);
 }
 
-AST_t* parseExpression(parser_t* parser) {
-
+AST_t* parseFile(parser_t* parser) {
+    AST_t* ast = initAST(AST_)
+    while (parser->token->type != TOKEN_EOF) {
+        list_t* children = initList(sizeof(struct AST_STRUCT));
+        listPush(children, parseFunction(parser));
+    }
 }
 
-AST_t* parseStatement(parser_t* parser) {
-    if (strcmp(parser->token->value, "back") == 0) {
-        AST_t* ast = initAST(AST_STATEMENT_RETURN);
-        parserEat(parser, TOKEN_ID);
-        ast->value = parseExpression(parser);
-        return ast;
-    }
-    else if (parser->token->type == TOKEN_IF) {
-        AST_t* ast = initAST(AST_STATEMENT_IF);
-        parserEat(parser, TOKEN_IF);
-        ast->value = parseExpression(parser);
-        return ast;
-    }
-    else if (parser->token->type == TOKEN_ELSE_IF){
-        AST_t* ast = initAST(AST_STATEMENT_ELSE_IF);
-        parserEat(parser, TOKEN_ELSE_IF);
-        ast->value = parseExpression(parser);
-        return ast;
-    }
-    else if (parser->token->type == TOKEN_CHAIN) {
-        lexerSkipWhiteSpace(parser->lexer);
-        lexerSkipComment(parser->lexer);
-        if (parser->lexer->c == '{') {
-            AST_t* ast = initAST(AST_STATEMENT_ELSE);
-            parserEat(parser, TOKEN_CHAIN);
-            ast->value = parseExpression(parser);
-            return ast;
-        }
-        else if (parser->lexer->c == '(') {
-            AST_t* ast = initAST(AST_STATEMENT_CASE);
-            parserEat(parser, TOKEN_CHAIN);
-            ast->value = parseExpression(parser);
-            return ast;
-        }
-        else{
-            return parseExpression(parser);
-        }
-    }    
-    else {
-        return parseExpression(parser);
-    }
+AST_t* parseFunction(parser_t* parser) {
 
-}
-
-AST_t* parseCompound(parser_t* parser) {
-    unsigned char shouldClose = 0;
-
-    if (parser->token->type == TOKEN_LEFT_BRACE) {
-        parserEat(parser, TOKEN_LEFT_BRACE);
-        shouldClose = 1;
-    }
-
-    AST_t* compound = initAST(AST_COMPOUND);
-
-    while (parser->token->type != TOKEN_EOF && parser->token->type != TOKEN_RIGHT_BRACE) {
-
-        listPush(compound->children, parseStatement(parser));
-
-        if (parser->token->type == TOKEN_SEMICOLON)
-        parserEat(parser, TOKEN_SEMICOLON);
-    }
-
-    if (shouldClose) {
-        parserEat(parser, TOKEN_RIGHT_BRACE);
-    }
-
-    return compound;
 }
